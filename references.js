6b53b82f6afcfdbe533159b8a373384ce320c784
Abhishek Kumar <abhishek.kumar@mountblue.io>
2018-05-30 23:09:32 +0530

    update the modular functionality

--- references.js
+++ references.js
@@ -1,47 +1,30 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/blog_demo_2')
+mongoose.connect('mongodb://localhost/blog_demo_2');
 
-// POST - title, content
-const postSchema = new mongoose.Schema({
-    title: String,
-    content: String
+const Post = require('./models/post');
+const User = require('./models/user');
+
+Post.create({
+    title: "Post 4",
+    content: 'This is content 4'
+}, (err, post)=> {
+    if(err)
+        console.log(err);
+    else {
+        User.findOne({email: 'bob@gmail.com'}, (err, foundUser)=>{
+            if(err)
+                console.log(err)
+            else
+                foundUser.posts.push(post);
+                foundUser.save((err, data)=>{
+                    if(err)
+                        console.log(err);
+                    else
+                        console.log(data);
+                });
+        });
+    }
 });
-
-const Post = mongoose.model('Post', postSchema);
-
-// USER - email, name
-const userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Post'
-    }]
-});
-
-const User = mongoose.model('User', userSchema);
-
-// Post.create({
-//     title: "Post 3",
-//     content: 'This is content 3'
-// }, (err, post)=> {
-//     if(err)
-//         console.log(err);
-//     else {
-//         User.findOne({email: 'bob@gmail.com'}, (err, foundUser)=>{
-//             if(err)
-//                 console.log(err)
-//             else
-//                 foundUser.posts.push(post);
-//                 foundUser.save((err, data)=>{
-//                     if(err)
-//                         console.log(err);
-//                     else
-//                         console.log(data);
-//                 });
-//         });
-//     }
-// });
 
 // User.create({
 //     email: 'bob@gmail.com',
@@ -52,10 +35,10 @@ const User = mongoose.model('User', userSchema);
 //Find user
 //find all posts for that user
 
-User.find({email: 'bob@gmail.com'}).populate('posts').exec((err, user)=>{
-    if(err)
-        console.log(err);
-    else {
-        console.log(user);
-    }
-});
+// User.find({email: 'bob@gmail.com'}).populate('posts').exec((err, user)=>{
+//     if(err)
+//         console.log(err);
+//     else {
+//         console.log(user);
+//     }
+// });
